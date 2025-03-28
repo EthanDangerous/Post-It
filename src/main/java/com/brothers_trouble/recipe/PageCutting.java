52bb973c8e1fb6a5164d90bcd873b28b2f0a9cac
brothers_trouble <ethan.baraf@gmail.com>
2025-03-27 20:15:18 -0700

    trying to figure out custom crafts

--- src/main/java/com/brothers_trouble/recipe/PageCutting.java
+++ src/main/java/com/brothers_trouble/recipe/PageCutting.java
@@ -1,12 +1,17 @@
 package com.brothers_trouble.recipe;
 
+import com.brothers_trouble.item.PostItItem;
+import com.brothers_trouble.registration.ItemRegistry;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.CraftingBookCategory;
 import net.minecraft.world.item.crafting.CraftingInput;
 import net.minecraft.world.item.crafting.CustomRecipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.item.Items;
 
 public class PageCutting extends CustomRecipe {
     public PageCutting(CraftingBookCategory category) {
@@ -15,17 +20,54 @@ public class PageCutting extends CustomRecipe {
 
     @Override
     public boolean matches(CraftingInput craftingInput, Level level) {
+        int p = 0;
+        int sh = 0;
+        int sl = 0;
+        for(int i = 0; i<craftingInput.size(); i++){
+            ItemStack item = craftingInput.getItem(i);
+            if (!item.is(Items.AIR)) {
+                if(item.is(Items.PAPER)){
+                    p++;
+                }
+                if(item.is(Items.SHEARS)){
+                    sh++;
+                }
+                if(item.is(Items.SLIME_BALL)){
+                    sl++;
+                }
+            }
+        }
+        if(p == 1 && sh == 1 && sl == 1){
+            return true;
+        }
         return false;
+    }
+
+    @Override
+    public NonNullList<ItemStack> getRemainingItems(CraftingInput input) {
+        NonNullList<ItemStack> remains = super.getRemainingItems(input);
+
+        for(int i = 0; i<input.size(); i++){
+//            ItemStack tool = ItemStack.EMPTY;
+            ItemStack item = input.getItem(i);
+            if (item.is(Items.SHEARS)) {
+                item.setDamageValue(item.getDamageValue()-1);
+//                tool = item;
+                remains.set(i, item);
+                return remains;
+            }
+        }
+        return super.getRemainingItems(input);
     }
 
     @Override
     public ItemStack assemble(CraftingInput craftingInput, HolderLookup.Provider provider) {
-        return null;
+        return new ItemStack(PostItItem);
     }
 
     @Override
     public boolean canCraftInDimensions(int i, int i1) {
-        return false;
+        return i == 2 && i1 == 2;
     }
 
     @Override
